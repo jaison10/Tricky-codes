13ee14eaf7eb502a4745f6ea691367f12404867e
Jaison DSouza B <jaisonj1010@gmail.com>
2020-07-14 11:38:36 +0530

    Create swap-values.js

---
+++ my-codes/swap-values.js
@@ -0,0 +1,10 @@
+let first = 'Jaison';
+let second = 'Dsouza';
+
+// swapping
+// This is the method of destructuring the array. 'second' is assigned with first value and 'first' is assigned with second value.
+// The first 2 variables are from somewhere. You need to specify variables when you destructure an array.
+
+[second, first] = [first, second];
+
+console.log(first, second);
