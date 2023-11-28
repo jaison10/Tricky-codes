188e0be91874c5ba9b3b804b5af4cac2d50893dd
Jaison DSouza B <jaisonj1010@gmail.com>
2023-11-28 20:38:04 +0530

    Create returnSomethingWithIfCond.js

---
+++ my-codes/returnSomethingWithIfCond.js
@@ -0,0 +1,8 @@
+if(true && 'Hello')   //this will return Hello.
+if(false && 'Hello')  //this will return false.
+
+// this can be used if there is no else part in your code and want to return something based on a condition.
+
+//Ex:
+let items = [];
+if(items.length === 0 && 'No items found')
